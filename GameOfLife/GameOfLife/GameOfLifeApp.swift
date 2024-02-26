b53d92745f8bb12179b694607ce75d030301d7b7
RaphaelIniesta <raphael_iniesta@yahoo.com>
2024-02-26 14:51:25 -0300

    Criar projeto

---
+++ GameOfLife/GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Raphael Iniesta Reis on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
