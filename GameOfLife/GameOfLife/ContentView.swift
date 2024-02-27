1c2bde43158a8655adf91e266f3ea4c5e3b147ac
RaphaelIniesta <raphael_iniesta@yahoo.com>
2024-02-27 14:30:48 -0300

    Adicionar matriz em SwiftUI

--- GameOfLife/GameOfLife/ContentView.swift
+++ GameOfLife/GameOfLife/ContentView.swift
@@ -8,12 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    func criaMatriz(cols: Int, rows: Int) -> [[Bool]] {
+        var matriz: [[Bool]] = []
+        var linha: [Bool] = []
+        
+        for _ in (0...rows-1) {
+            for _ in 0...cols-1 {
+                let valor = Int.random(in: 0...1)
+                
+                if(valor == 1) {
+                    linha.append(true)
+                } else {
+                    linha.append(false)
+                }
+            }
+            matriz.append(linha)
+            linha.removeAll()
+        }
+        
+        return matriz
+    }
+    
+    @State var matriz: [[Bool]] = []
+    
+    var cols = 5
+    var rows = 5
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Button("Matriz") {
+                matriz = criaMatriz(cols: cols, rows: rows)
+                for i in (0...matriz.count-1) {
+                    for j in (0...matriz[i].count-1) {
+                        print(matriz[i][j], terminator: "")
+                    }
+                    print("\n")
+                }
+            }
+            
+            ForEach(0..<matriz.count, id: \.self) { i in
+                HStack {
+                    ForEach(0..<matriz[i].count, id: \.self) { j in
+                        if (matriz[i][j]) {
+                            RoundedRectangle(cornerRadius: 7)
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.cyan)
+                        } else {
+                            RoundedRectangle(cornerRadius: 7)
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+            }
+            
         }
         .padding()
     }
